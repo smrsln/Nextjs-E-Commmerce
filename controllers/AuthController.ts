e814e8554ae9b733dde0e59575dac95267fd1822
smrsln <oasimarslan@gmail.com>
2023-10-11 15:21:23 +0300

    JWT based session has been creating...

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -5,6 +5,7 @@ import { User, Account, Profile } from "next-auth";
 import { UserDocument } from "@/models/User";
 import { FacebookProfile } from "next-auth/providers/facebook";
 import { GoogleProfile } from "next-auth/providers/google";
+import jwt, { Jwt, Secret } from "jsonwebtoken";
 
 export class AuthController {
   private authService: AuthService;
@@ -41,4 +42,16 @@ export class AuthController {
   async signOut(req: NextApiRequest, res: NextApiResponse) {
     await this.authService.signOut(req, res);
   }
+
+  //Will be moving into AuthService START
+  async encodeToken(token: Jwt, secret: Secret): Promise<string> {
+    const encodedToken = jwt.sign(token, secret);
+    return encodedToken;
+  }
+
+  async decodeToken(token: string, secret: string): Promise<any> {
+    const decodedToken = jwt.verify(token, secret);
+    return decodedToken;
+  }
+  // will be moving into AuthService END
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,28 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import NextAuth, { Account, User, Profile } from "next-auth";
+import NextAuth, { Account, User, Profile, Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 import FacebookProvider, {
   FacebookProfile,
 } from "next-auth/providers/facebook";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthController } from "@/controllers/AuthController";
+import { UserModel } from "@/models/User";
 
 const authController = new AuthController();
 
 export default NextAuth({
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const { email, password } = credentials;
-        const user = await authController.signInWithPassword(email, password);
-        return user ? { ...user, email } : null;
-      },
-    }),
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     email: { label: "Email", type: "text", placeholder: "jsmith" },
+    //     password: { label: "Password", type: "password" },
+    //   },
+    //   async authorize(credentials) {
+    //     const { email, password } = credentials;
+    //     const user = await authController.signInWithPassword(email, password);
+    //     return user ? { ...user, email } : null;
+    //   },
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
@@ -33,16 +35,28 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async signIn(params) {
-      const { user, account, profile } = params;
+    async session({ session, token }: { session: Session; token: JWT }) {
+      try {
+        if (session.user) {
+          const sessionUser = await UserModel.findOne({
+            email: session.user.email,
+          });
+          session.user.id = sessionUser?._id.toString();
+          session.user = { ...session.user, id: sessionUser?._id.toString() };
+        }
+        return session;
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error while creating session");
+      }
+    },
+    async signIn({ user, account, profile }) {
       if (account?.provider === "google") {
         if ((profile as GoogleProfile)?.email_verified) {
-          const UserDocument = await authController.googleSignIn(
+          const user = await authController.googleSignIn(
             profile as GoogleProfile
           );
-          return UserDocument
-            ? { ...UserDocument, email: profile?.email }
-            : null;
+          return user ? { ...user, email: profile?.email } : null;
         }
       } else if (account?.provider === "facebook") {
         if ((profile as FacebookProfile)?.verified) {
@@ -64,10 +78,48 @@ export default NextAuth({
       }
       return null;
     },
-    async signUp(user: User, account: Account, profile: Profile) {
-      const { email, password } = user;
-      const user = await authController.signInWithPassword(email, password);
-      return user ? { ...user, email } : null;
+    async jwt({
+      token,
+      user,
+      account,
+      profile,
+      isNewUser,
+    }: {
+      token: JWT;
+      user: User;
+      account: Account;
+      profile: Profile;
+      isNewUser: boolean;
+    }): Promise<{
+      token: JWT;
+      user: User;
+      account: Account;
+      profile: Profile;
+      isNewUser: boolean;
+    }> {
+      if (user) {
+        token.id = user.id;
+      }
+      return { token, user, account, profile, isNewUser };
+    },
+    //I will make next-auth credential sign in work later
+
+    // async signUp(user: User, account: Account, profile: Profile) {
+    //   const { email, password } = user;
+    //   const user = await authController.signInWithPassword(email, password);
+    //   return user ? { ...user, email } : null;
+    // },
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    encode: async ({ token, secret }) => {
+      const encodedToken = await authController.encodeToken(token, secret);
+      return encodedToken;
+    },
+    decode: async ({ token, secret }) => {
+      const decodedToken = await authController.decodeToken(token, secret);
+      return decodedToken;
     },
   },
   session: {
@@ -75,7 +127,7 @@ export default NextAuth({
   },
   pages: {
     signIn: "/auth/signin",
-    signOut: "/auth/signout",
-    error: "/auth/error",
+    //signOut: "/auth/signout",
+    //error: "/auth/error",
   },
 });
